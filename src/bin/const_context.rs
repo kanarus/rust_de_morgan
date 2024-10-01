feb53bb913ecf6b21116014e572d689d92ab82e6
kana-rus <kanarus786@gmail.com>
2024-10-02 05:09:10 +0900

    add const_context version

---
+++ src/bin/const_context.rs
@@ -0,0 +1,41 @@
+macro_rules! static_assert {
+    ($condition:expr) => {
+        {
+            const _: bool = $condition;
+            const _: [(); $condition as usize] = [(); true as usize];
+        }
+    };
+}
+
+macro_rules! for_all_cominations_of_two_bools {
+    (($P:ident, $Q:ident) => $proc:expr) => {
+        for_all_cominations_of_two_bools! {
+            @for ($P, $Q) in checked[
+                (true,  true ),
+                (true,  false),
+                (false, true),
+                (false, false)
+            ] {
+                $proc
+            }
+        }
+    };
+    (@for ($P:ident, $Q:ident) in checked[$( ($p:literal, $q:literal) ),*] $proc:expr) => {
+        fn __assert_exausted__(p: bool, q: bool) {
+            match (p, q) {$(
+                ($p, $q) => {
+                    const $P: bool = $p;
+                    const $Q: bool = $q;
+                    $proc
+                },
+            )*}
+        }
+    };
+}
+
+const fn main() {
+    for_all_cominations_of_two_bools! {(P, Q) => {
+        static_assert!(!(P || Q) == (!P && !Q));
+        static_assert!(!(P && Q) == (!P || !Q));
+    }}
+}
